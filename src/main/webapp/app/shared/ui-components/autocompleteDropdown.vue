b884635d840e481d08382350b8599b4d98eaf503
Krit <kritdevelop@gmail.com>
2020-09-11 17:25:53 +0700

    autocompleteDropdown - create component

---
+++ src/main/webapp/app/shared/ui-components/autocompleteDropdown.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dropdown" :class="{'open' : open}">
+    <input type="text" class="form-control" :class="classProperty" :required="required"
+      ref="search"
+      :placeholder="placeholder"
+      v-model="searchText"
+      @input="searchChanged"
+      @keydown.enter="suggestionSelected(matches[highlightIndex])"
+      @blur="suggestionSelected(matches[highlightIndex])"
+      @keydown.down="down"
+      @keydown.up="up"
+      @keydown.esc="setOpen(false)"
+    />
+    <!-- <a class="toggle" @mousedown.prevent @click="setOpen(!open)">
+      <span class="arrow-up">▲</span>
+      <span class="arrow-down">▼</span>
+    </a> -->
+    <ul class="suggestion-list">
+      <li v-for="(suggestion, index) in matches" :key="index"
+        :class="{'active' : index === highlightIndex}"
+        @mousedown.prevent
+        @click="suggestionSelected(suggestion)"
+      >
+        {{ suggestion[1][displayField] }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+    @import "./autocompleteDropdown.css";
+</style>
+
+<script lang="ts" src="./autocompleteDropdown.component.ts">
+</script>
